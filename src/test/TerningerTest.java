52a684ca6c11b63df747dcfc3b9a59ab6f45beb2
wulff-dtu <s185036@student.dtu.dk>
2018-10-10 11:15:13 +0200

    Har tilføjet lidt dokumentation til testen af terningerne.

--- src/test/TerningerTest.java
+++ src/test/TerningerTest.java
@@ -14,33 +14,59 @@ class TerningerTest {
 
         Terninger terninger = new Terninger();
 
+        // Indtast her, hvor mange testkast, du vil teste, samt hvor stor
+        // en difference fra den teoretiske sandsynlighed, du vil tillade (i procent).
+
         int antalTestSlag = 1000;
         double tilladtProcentDifference = 0.02;
 
-        int[] forekomstAfSummer = new int[11];
-        int forekomstAfEns = 0;
-        double forekomstEnsProcent;
-        double forekomstEnsProcentDifference;
-        double[] forekomsterIprocent = new double[11];
-        double[] procentDifference = new double[11];
-        boolean[] godkendtDifferenceSummer = new boolean[11];
-        boolean godkendtDifferenceEns;
-        double[] sandsynlighedForSummer = {1.0/36.0, 2.0/36.0, 3.0/36.0, 4.0/36.0, 5.0/36.0, 6.0/36.0, 5.0/36.0, 4.0/36.0, 3.0/36.0, 2.0/36.0, 1.0/36.0};
-        double sandsynlighedForEns = 6.0/36.0;
+        //med "f" menes "forekomst af". Med fx "fSummer" menes der "forekomster af summer".
+        //med "s" menes der "sandsynlighed for". Med fx "sSummer" menes der "teoretisk sandsynlighed for summer".
+
+        int[] fSummer = new int[11];
+        int fEns = 0;
+        double fEnsProcent;
+        double fEnsProcentDiff;
+        double[] fSummerProcent = new double[11];
+        double[] fSummerProcentDiff= new double[11];
+        boolean[] gDiffSummer = new boolean[11];
+        boolean gDiffEns;
+        double[] sSummer = {1.0/36.0, 2.0/36.0, 3.0/36.0, 4.0/36.0, 5.0/36.0, 6.0/36.0, 5.0/36.0, 4.0/36.0, 3.0/36.0, 2.0/36.0, 1.0/36.0};
+        double sEns = 6.0/36.0;
+
+        //Vi kaster terningerne og indsamler data
 
         for (int i = 0; i < antalTestSlag; i++) {
 
             terninger.slaaTerninger();
             int sum = terninger.getSum();
 
-            forekomstAfSummer[sum - 2]++;
-            if (terninger.erEns()) forekomstAfEns++;
+            fSummer[sum - 2]++;
+            if (terninger.erEns()) fEns++;
 
         }
 
-        forekomstEnsProcent = (double) forekomstAfEns / antalTestSlag;
-        forekomstEnsProcentDifference = sandsynlighedForEns - forekomstEnsProcent;
-        godkendtDifferenceEns = forekomstEnsProcentDifference < tilladtProcentDifference;
+        // vi regner på data
+
+        fEnsProcent = (double) fEns / antalTestSlag;
+        fEnsProcentDiff = sEns - fEnsProcent;
+        gDiffEns = fEnsProcentDiff < tilladtProcentDifference;
+
+        for (int i = 0; i < 11; i++) {
+            fSummerProcent[i] = (double) fSummer[i] / antalTestSlag;
+            fSummerProcentDiff[i] = sSummer[i] - fSummerProcent[i];
+            gDiffSummer[i] = Math.abs(fSummerProcentDiff[i]) < tilladtProcentDifference;
+        }
+
+        //Vi tester om udfaldet kan godkendes
+
+        for (boolean godkendelse : gDiffSummer) {
+            assertTrue(godkendelse);
+        }
+
+        assertTrue(gDiffEns);
+
+        //Vi printer udfaldet af testen til konsollen
 
         DecimalFormat dc = new DecimalFormat("#.####");
 
@@ -49,23 +75,12 @@ class TerningerTest {
         System.out.format("%15s%15s%15s%15s%15s%15s\n", dataTyper);
 
         for (int i = 0; i < 11; i++) {
-            forekomsterIprocent[i] = (double) forekomstAfSummer[i] / antalTestSlag;
-            procentDifference[i] = sandsynlighedForSummer[i] - forekomsterIprocent[i];
-            godkendtDifferenceSummer[i] = Math.abs(procentDifference[i]) < tilladtProcentDifference;
-            String[] tabelLinje = {i + 2 + "", forekomstAfSummer[i] + "", forekomsterIprocent[i] + "%", dc.format(sandsynlighedForSummer[i]) + "%", dc.format(procentDifference[i]) + "%", godkendtDifferenceSummer[i] + ""};
+            String[] tabelLinje = {i + 2 + "", fSummer[i] + "", fSummerProcent[i] + "%", dc.format(sSummer[i]) + "%", dc.format(fSummerProcentDiff[i]) + "%", gDiffSummer[i] + ""};
             System.out.format("%15s%15s%15s%15s%15s%15s\n", tabelLinje);
         }
 
-
-
-        String[] tabelLinje = {"Ens", forekomstAfEns + "", dc.format(forekomstEnsProcent) + "%", dc.format(sandsynlighedForEns) + "%", dc.format(forekomstEnsProcentDifference) + "%", godkendtDifferenceEns + ""};
+        String[] tabelLinje = {"Ens", fEns + "", dc.format(fEnsProcent) + "%", dc.format(sEns) + "%", dc.format(fEnsProcentDiff) + "%", gDiffEns + ""};
         System.out.format("%15s%15s%15s%15s%15s%15s\n", tabelLinje);
-
-        for (boolean godkendelse : godkendtDifferenceSummer) {
-            assertTrue(godkendelse);
-        }
-
-        assertTrue(godkendtDifferenceEns);
 
     }
 }
